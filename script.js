d92a292a66975d62a94ab4a3a16e63ea0e7b5099
Sacha Sedriks <sachased@gmail.com>
2020-03-29 09:32:58 -0700

    init '_img' directory. all images will be sourced from IF WE DON'T, REMEMBER ME > https://iwdrm.tumblr.com/

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// In-memory database of questions
+const QUESTIONS = [
+
+
+
+
+];
+
+// Create your initial store
+const STORE = {
+  // Current question
+  // User's answer choice(s)
+  // Current view
+  // Score? Anything else?
+};

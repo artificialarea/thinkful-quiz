d881cb6166fc3abc620132a10e7b526875049d6f
Sacha Sedriks <sachased@gmail.com>
2020-03-29 15:21:23 -0700

    added questions to store.js and associated gif animations (which are mostly .webp files).

--- index.js
+++ index.js
@@ -5,3 +5,46 @@
 
 
 
+
+// GUIDANCE //////////////////////////////////////////////
+// from https://thinkful.slides.com/thinkful/quiz-app#/9
+
+// 1 VIEWS AND RENDERING /////////////////////////////////
+
+function render() {
+  if (store.view === 'start') {
+    $('.intro').show();
+    $('.quiz').hide();
+    $('.status').hide();
+  } else if (store.view === 'quiz') {
+    $('.intro').hide();
+    $('.quiz').show();
+    $('.status').show();
+  }
+}
+
+
+// 2 EXAMPLE APP STRUCTURE ///////////////////////////////
+
+// Template generators
+function generateAnswerList(answers) {}
+
+// Rendering functions
+function renderQuestionText() {}
+
+// Event handlers
+function handleAnswerSubmitted() {
+  $('.user-controls').on('click', '.submit-answer', () => {
+    // Retrieve answer identifier of user-checked radio button
+    // Perform check: User answer === Correct answer?
+    // Update STORE and render appropriate section
+  });
+}
+
+
+// After DOM has loaded, initialize
+$(function(){
+    handleAnswerSubmitted();
+});
+
+
--- store.js
+++ store.js
@@ -2,16 +2,99 @@
 
 // In-memory database of questions
 const QUESTIONS = [
-
-
-
-
+  // 1
+  {
+    question: 'In the film \'Fight Club\', The Narrator (Edward Norton) befriends Robert \'Bob\' Paulsen (Meatloaf) in which support group?',
+    choices: [
+      'A support group for men with testicular cancer.',
+      'A support group for insomniacs.',
+      'A support group for recovering alcoholics.',
+      'A support group for the bereaved.',
+    ],
+    answer: 'A support group for men with testicular cancer.',
+    image: 'IWDRM_fightClub.webp',
+  },
+  // 2
+  {
+    question: 'In the film \'The Conversation\', what is Harry Caul’s (Gene Hackman) occupation?',
+    choices: [
+      'He’s a surveillance expert.',
+      'He’s a jazz musician.      ',
+      'He’s a trial laywer.',
+      'He’s a teacher.',
+    ],
+    answer: 'He’s a surveillance expert.',
+    image: 'IWDRM_theConversation.gif',
+  },
+  // 3
+  {
+    question: 'In the film \'The Life Aquatic with Steve Zissou\', what is the name of Steve Zissou’s (Bill Murray) aging research vessel?',
+    choices: [
+      'Belefonte',
+      'Cousteau',
+      'Wes Coast',
+      'Pequod',
+    ],
+    answer: 'Belefonte',
+    image: 'IWDRM_theLifeAquatic.webp',
+  },
+  // 4
+  {
+    question: 'In the film ‘2001: A Space Odyssey’, what are the final words that Dr. Dave Bowman (Keir Dullea) says?',
+    choices: [
+      '“Oh my god, it’s full of stars…”',
+      '“HAL! Can you hear me HAL!?!”',
+      '“Also sprach Zarathustra.”',
+      '“This monolith looks friendly.”',
+    ],
+    answer: '“Oh my god, it’s full of stars…”',
+    image: 'IWDRM_2001.webp',
+  },
+  // 5
+  {
+    question: 'In the film “Clockwork Orange”, the experimental psychological conditioning technique employed on young droogie Alex (Malcolm McDowell) is referred to as what?',
+    choices: [
+      'The Ludovico Technique',
+      'Cognitive Behavioural Therapy',
+      'The Pavlovian Theory',
+      'The Ellsberg Paradox',
+    ],
+    answer: 'The Ludovico Technique',
+    image: 'IWDRM_clockworkOrange.webp',
+  },
+  // 6
+  {
+    question: 'In the film “Old Country for Old Men”, what is the method Anton Chigurh (Javier Bardem) uses for deciding whether or not to kill someone?',
+    choices: [
+      'He flips a coin.',
+      'He looks deeply into their eyes.',
+      'He smells them.',
+      'He has a drink of milk first, to contemplate.',
+    ],
+    answer: 'He flips a coin.',
+    image: 'IWDRM_NoCountryForOldMen.webp',
+  },
+  /* template
+  {
+    image: ''
+    question: '',
+    choices: [
+      '',
+      '',
+      '',
+      '',
+    ],
+    answer: '',
+    image: '',
+  }, */
 ];
 
 // Create your initial store
 const STORE = {
   // Current question
+  currentQuestion: 0,
   // User's answer choice(s)
   // Current view
   // Score? Anything else?
+  score: 0,
 };
